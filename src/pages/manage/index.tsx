d9892747aee44ca934692e143757d4c90289b2a9
hoho2017 <1063907253@qq.com>
2020-11-10 14:21:23 +0800

    🔖 test commit

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -6,39 +6,14 @@ import FreeManageItem from '@/components/FreeManageItem';
 
 import styles from './index.less';
 
-const RadioGroup = Radio.Group;
 
 interface PageProps extends ConnectProps {
   manage: ManageModelState;
 }
 
-const manageType = [
-  { key: 0, value: '全部' },
-  { key: 1, value: '战士' },
-  { key: 2, value: '法师' },
-  { key: 3, value: '坦克' },
-  { key: 4, value: '刺客' },
-  { key: 5, value: '射手' },
-  { key: 6, value: '辅助' },
-];
-
 const Manage: FC<PageProps> = ({ manage, dispatch }) => {
   const { manages = [], filterKey = 0, freemanages = [], itemHover = 0 } = manage;
-  const onChange = (e: RadioChangeEvent) => {
-    dispatch!({
-      type: "manage/save", payload: {
-        filterKey: e.target.value
-      }
-    })
-  };
-  const onItemHover = (index: number) => {
-    dispatch!({
-      type: 'manage/save',
-      payload: {
-        itemHover: index
-      },
-    });
-  }
+
 
   return (
     <div className={styles.normal}>
